d4a921f2b3efea0a3c6e9be54d2953dd4ee633c5
Restyled.io <commits@restyled.io>
2024-02-16 17:50:47 +0000

    Restyled by clang-format

--- src/main/java/com/jnape/palatable/lambda/functions/builtin/fn1/Next.java
+++ src/main/java/com/jnape/palatable/lambda/functions/builtin/fn1/Next.java
@@ -1,29 +1,30 @@
 package com.jnape.palatable.lambda.functions.builtin.fn1;
 
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+
 import com.jnape.palatable.lambda.functions.Fn1;
 import com.jnape.palatable.lambda.traversable.Traversable;
 import com.jnape.palatable.lambda.zipper.Zipper;
 
-import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+public final class Next<A, TA extends Traversable<A, ?>>
+    implements Fn1<Zipper<A, TA>, Zipper<A, TA>> {
 
-public final class Next<A, TA extends Traversable<A, ?>> implements Fn1<Zipper<A, TA>, Zipper<A, TA>> {
+  private static final Next INSTANCE = new Next();
 
-    private static final Next INSTANCE = new Next();
+  private Next() {}
 
-    private Next() {
-    }
+  @Override
+  public Zipper<A, TA> apply(Zipper<A, TA> zipper) {
+    return zipper.match(Zipper::top, n -> n._2().apply(nothing()));
+  }
 
-    @Override
-    public Zipper<A, TA> apply(Zipper<A, TA> zipper) {
-        return zipper.match(Zipper::top, n -> n._2().apply(nothing()));
-    }
+  @SuppressWarnings("unchecked")
+  public static <A, TA extends Traversable<A, ?>> Next<A, TA> next() {
+    return INSTANCE;
+  }
 
-    @SuppressWarnings("unchecked")
-    public static <A, TA extends Traversable<A, ?>> Next<A, TA> next() {
-        return INSTANCE;
-    }
-
-    public static <A, TA extends Traversable<A, ?>> Zipper<A, TA> next(Zipper<A, TA> zipper) {
-        return Next.<A, TA>next().apply(zipper);
-    }
+  public static <A, TA extends Traversable<A, ?>> Zipper<A, TA>
+  next(Zipper<A, TA> zipper) {
+    return Next.<A, TA>next().apply(zipper);
+  }
 }
